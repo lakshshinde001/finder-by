0e17a436fb6c74fd8d64a2deeb2c71ef5562bce8
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-03-30 10:53:31 +0530

    added getAlluser, getSingleUser, updateUserRoles, deleteUserRoles, updateProfile, updatePaasword etc Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,6 +6,7 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors.js');
 const User = require('../models/userModel.js');
 const sendToken = require('../utils/jwtToken.js');
 const sendMail = require('../utils/sendMail.js')
+const crypto = require('crypto')
 
 
 
@@ -104,3 +105,168 @@ exports.forgotpassword = catchAsyncErrors(async (req, res, next) => {
     }
 
 })
+
+// Reset password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+    
+    const resetPasswordToken = crypto
+     .createHash('sha256')
+     .update(req.params.token)
+     .digest('hex');
+
+     const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    });
+
+    if(!user){
+        return next(new ErrorHandler("Password reset token is invalid or has expired", 400));
+    }
+
+    if(req.body.password !== req.body.comfirmPassword ){
+        return next(new ErrorHandler("Password does not match", 400));
+    }
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    sendToken(user, 200, res);
+});
+
+// Get User Detail
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+  
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  });
+
+  // update User password
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+  
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+  
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Old password is incorrect", 400));
+    }
+  
+    if (req.body.newPassword !== req.body.confirmPassword) {
+      return next(new ErrorHandler("password does not match", 400));
+    }
+  
+    user.password = req.body.newPassword;
+  
+    await user.save();
+  
+    sendToken(user, 200, res);
+  });
+  
+  // update User Profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+      name: req.body.name,
+      email: req.body.email,
+    };
+    // cloudinary code
+  
+    // if (req.body.avatar !== "") {
+    //   const user = await User.findById(req.user.id);
+  
+    //   const imageId = user.avatar.public_id;
+  
+    //   await cloudinary.v2.uploader.destroy(imageId);
+  
+    //   const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    //     folder: "avatars",
+    //     width: 150,
+    //     crop: "scale",
+    //   });
+  
+    //   newUserData.avatar = {
+    //     public_id: myCloud.public_id,
+    //     url: myCloud.secure_url,
+    //   };
+    // }
+  
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+    });
+  });
+  
+
+  // Get all users(admin)
+exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
+    const users = await User.find();
+  
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  });
+  
+  // Get single user (admin)
+  exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+  
+    if (!user) {
+      return next(
+        new ErrorHandler(`User does not exist with Id: ${req.params.id}`)
+      );
+    }
+  
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  });
+
+  // update User Role -- Admin
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+      name: req.body.name,
+      email: req.body.email,
+      role: req.body.role,
+    };
+  
+    await User.findByIdAndUpdate(req.params.id, newUserData, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  
+    res.status(200).json({
+      success: true,
+    });
+  });
+  
+  // Delete User --Admin
+  exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+  
+    if (!user) {
+      return next(
+        new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
+      );
+    }
+    
+    // cludinary code 
+    // const imageId = user.avatar.public_id;
+  
+    // await cloudinary.v2.uploader.destroy(imageId);
+  
+    await user.remove();
+  
+    res.status(200).json({
+      success: true,
+      message: "User Deleted Successfully",
+    });
+  });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { registerUser, loginUser, logout, forgotpassword } = require('../controllers/userController');
+const { registerUser, loginUser, logout, forgotpassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUser, getSingleUser, updateUserRole, deleteUser } = require('../controllers/userController');
+const { isAuthenticatedUser, authorizedRoles } = require('../middlewares/auth');
+const { getAllProducts } = require('../controllers/productControllers');
 
 const router = express.Router();
 
@@ -7,4 +9,16 @@ router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logout)
 router.route('/forgotpassword').post(forgotpassword);
+
+router.route('/resetpassword:token').put(resetPassword)
+router.route('/me').get(isAuthenticatedUser,  getUserDetails);
+router.route('/updatepassword').post(isAuthenticatedUser, updatePassword)
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizedRoles('admin'), getAllUser)
+
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizedRoles('admin'), getSingleUser)
+.put(isAuthenticatedUser, authorizedRoles('admin'), updateUserRole)
+.delete(isAuthenticatedUser, authorizedRoles('admin'), deleteUser);
+
 module.exports = router;
